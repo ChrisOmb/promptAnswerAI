bf24047f67583514e37ea827543d7732dc7b89a0
ChrisOmb <christopherombwori06@gmail.com>
2025-01-04 00:10:09 +0300

    logic but has issue with the api--to be resolved

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import openai
+
 import os   #THhis enables us to interact with our environmental variables hence we can access the API key
-import dotenv  #This is a library that allows us to load our environmental variables from a .env file
-import load_dotenv         # Does the same thing as the above line
+from dotenv  import load_dotenv         
 
 load_dotenv()
 
 open_ai_key = os.getenv("OPENAI_API_KEY")
+if not openai.api_key:
+    print("API key is missing or incorrect.")
+    exit()
 
 def get_completion (prompt, model="gpt-3.5-turbo"):
     try:
-        response = openai.ChatCompletion.create(
-            model = model,
-            messages =[{
-                'role': 'user', "content": prompt}
-            ],
-            temperature = 0
-            )
-        return response.choices[0].message['content']
+        response = openai.ChatCompletion.create(model = model,
+        messages =[{
+            'role': 'user', "content": prompt}
+        ],
+        temperature = 0)
+        return response.choices[0].message.content
     except Exception as e:
         return f"An error occurred: {e}"
+
+if __name__ == "__main__":
+    prompt = input("How can i help you dear user?")
+    ai_response = get_completion(prompt)
+    print("Your Answer is: ", ai_response)
